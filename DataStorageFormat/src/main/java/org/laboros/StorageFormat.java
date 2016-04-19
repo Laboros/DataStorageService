d5d0be8e889776745a82b8a6f471c7229339e334
svaduka <sainagaraju.vaduka@gmail.com>
2016-04-19 21:17:37 +0530

    Updated .gitIgnore and StorageFormat updated

--- DataStorageFormat/src/main/java/org/laboros/StorageFormat.java
+++ DataStorageFormat/src/main/java/org/laboros/StorageFormat.java
@@ -1,9 +1,8 @@
 package org.laboros;
 
-import org.apache.hadoop.io.WritableComparable;
 
 public interface StorageFormat {
 	
-	public WritableComparable parseData(final String inputData);
+	public Object parseData(final String inputData);
 
 }
